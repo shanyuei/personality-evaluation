aa34e1171d81c1290de093a8b940be281cf87d1e
阿广 <17712152745@163com>
2026-04-23 23:29:11 +0800

    fix: 更新Strapi API配置并预加载字体文件
    将Strapi API配置从本地环境切换回生产环境，并添加对Outfit字体文件的预加载以优化性能

--- app/app.vue
+++ app/app.vue
@@ -10,6 +10,20 @@
 import type { ToasterProps } from '@nuxt/ui';
 
 const toaster = ref<ToasterProps>({ position: 'top-center' })
+
+useHead({
+  link: [
+    { rel: 'preload', href: '/fonts/Outfit/Outfit-Thin.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' },
+    { rel: 'preload', href: '/fonts/Outfit/Outfit-ExtraLight.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' },
+    { rel: 'preload', href: '/fonts/Outfit/Outfit-Light.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' },
+    { rel: 'preload', href: '/fonts/Outfit/Outfit-Regular.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' },
+    { rel: 'preload', href: '/fonts/Outfit/Outfit-Medium.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' },
+    { rel: 'preload', href: '/fonts/Outfit/Outfit-SemiBold.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' },
+    { rel: 'preload', href: '/fonts/Outfit/Outfit-Bold.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' },
+    { rel: 'preload', href: '/fonts/Outfit/Outfit-ExtraBold.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' },
+    { rel: 'preload', href: '/fonts/Outfit/Outfit-Black.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' }
+  ]
+})
 </script>
 
 <style scoped>
